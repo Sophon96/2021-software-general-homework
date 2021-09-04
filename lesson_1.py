e945653b7dfadee05c0334e121c8ba87431b6cb3
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-09-04 08:48:12 -0700

    Finish Lesson 1

--- lesson_1.py
+++ lesson_1.py
@@ -1,8 +1,18 @@
 # Create a program in which the user creates 
 # a specific FRC team and store the following variables:
-	# Team Number (named team_number)
-	# Team Name (named team_name)
-	# Location (named location)
-	# Rookie Year (named rookie_year)
-	# Is Active (named is_active)
+#     Team Number (named team_number)
+#     Team Name (named team_name)
+#     Location (named location)
+#     Rookie Year (named rookie_year)
+#     Is Active (named is_active)
 # Be sure to store each variable as the correct type!
+
+if __name__ == '__main__':
+    team_number = int(input("Team number (int): "))
+    team_name = input("Team name (str): ")
+    location = input("Location (str): ")
+    rookie_year = int(input("Rookie year (int): "))
+    is_active = bool(int(input("Is active (bool 0, 1): ")))
+
+    for i in ['team_number', 'team_name', 'location', 'rookie_year', 'is_active']:
+        print(f'{i}: {eval(i)}')
